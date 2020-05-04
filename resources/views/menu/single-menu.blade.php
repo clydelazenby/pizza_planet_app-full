b152dede58f8e48b316cc7f08e5abd5536a20ec1
CLYDE LAZENBY <CLYDELAZENBY@GMAIL.COM>
2020-05-03 22:17:52 -0400

    added food categories page html and css

--- resources/views/menu/single-menu.blade.php
+++ resources/views/menu/single-menu.blade.php
@@ -6,9 +6,65 @@
 
 
 @section('content')
-<div class="portrait">
-    <img src="/img/pizzaplanetmenu.jpg">
-</div>
+<div id="single-menu-page">
+    <div class="row">
+        <div class="offset-md-2 col-md-8">
+             <div class="content-box">
+                 <div class="row">
+                    <div class="col-md-12">
+                    <h1>Pizzas<h1>
+                    </div>
+                     <div class="col-md-6">
+                             <div class="item">
+                                <div class="title">
+                                <h4>Galaxy Pizza<h4>
+                                <span class="price">$9</span>
+                             </div>
+                             <div class="description">
+                                <p> Crust, Pepperoni, Tomato, Cheese<p>
+                             </div>
+                            </div>
+                    </div>
+                     <div class="col-md-6">
+                             <div class="item">
+                                <div class="title">
+                                <h4>Meat Lovers<h4>
+                                <span class="price">$9</span>
+                             </div>
+                             <div class="description">
+                                <p>Crust, Pepperoni, Tomato, Ground Beef, Cheese<p>
+                             </div>
+                            </div>
+                    </div>
+                     <div class="col-md-6">
+                             <div class="item">
+                                <div class="title">
+                                <h4>Veggie Pizza<h4>
+                                <span class="price">$9</span>
+                             </div>
+                             <div class="description">
+                                <p> Choose toppings<p>
+                             </div>
+                            </div>
+                    </div>
+                     <div class="col-md-6">
+                             <div class="item">
+                                <div class="title">
+                                <h4>BBQ Chicken Pizza<h4>
+                                <span class="price">$9</span>
+                             </div>
+                             <div class="description">
+                                <p> Chicken, BBQ Sauce, Cheese<p>
+                             </div>
+                            </div>
+                    </div>
+
+                 </div>
+            </div>
+        </div>
+    </div>
+ </div>
+  
 
 
 @endsection
