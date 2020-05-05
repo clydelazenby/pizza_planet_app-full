fa44ae5c91524ff848d1f6634a0a40b8e84c64c6
CLYDE LAZENBY <CLYDELAZENBY@GMAIL.COM>
2020-05-04 22:09:26 -0400

    Created the users pages

--- resources/views/admin/users/all.blade.php
+++ resources/views/admin/users/all.blade.php
@@ -13,14 +13,14 @@
                 <div class="row">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                         <div class="page-header">
-                            <h2 class="pageheader-title">All Reservations</h2>
+                            <h2 class="pageheader-title">Users</h2>
                             <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
                             <div class="page-breadcrumb">
                                 <nav aria-label="breadcrumb">
                                     <ol class="breadcrumb">
                                         <li class="breadcrumb-item"><a href="/admin" class="breadcrumb-link">Dashboard</a></li>
                                     
-                                        <li class="breadcrumb-item active" aria-current="page">All Reservations</li>
+                                        <li class="breadcrumb-item active" aria-current="page">Users</li>
                                     </ol>
                                 </nav>
                             </div>
@@ -37,28 +37,29 @@
                         <!-- ============================================================== -->
                         <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
                             <div class="card">
-                                <h5 class="card-header">All  Reservations</h5>
+                                <h5 class="card-header">All Users</h5>
                                 <div class="card-body">
                                     <table class="table">
                                         <thead>
                                             <tr>
                                                 <th scope="col">id</th>
-                                                <th scope="col">Full Name</th>
                                                 <th scope="col">Email</th>
-                                                <th scope="col">Phone Number</th>
-                                                <th scope="col">Guests Total</th>
                                                 <th scope="col">Date Created</th>
+                                                <th scope="col">Edit</th>
+                                                <th scope="col">Delete</th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <tr>
                                                 <th scope="row">1</th>
-                                                <td>Billy Jones</td>
-                                                <td>Billy@gmail.com</td>
-                                                <td>919-919-9100</td>
-                                                <td>4</td>
-                                                <td>2/2/2020</td>
-                                                
+                                                <td>John Doe</td>
+                                                <td>2/2/20</td>
+                                                <td>
+                                                <a href="/admin/users/1/edit"><i class="far fa-edit"></i></a>
+                                                </td>
+                                                <td>
+                                                <a href="/admin/users/1/delete"onclick="if(!confirm('Are you sure you want to delete User?')){return false; }"><i class="far fa-trash-alt"></i>
+                                                </td>
                                             </tr>
                                          
                                         </tbody>
@@ -76,3 +77,4 @@
 @endsection
   
   
+ 
